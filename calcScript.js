ad76dc7070882087083c9632b8c890d9fe16ab7b
OneGiantTree <115727986+TheOneGiantTree@users.noreply.github.com>
2023-11-20 17:09:24 -0500

    Add basic function.

--- calcScript.js
+++ calcScript.js
@@ -1,8 +1,8 @@
-
-let displayValue = "000";
+let displayValue = "0";
 let num1;
 let num2;
 let operator;
+let answer;
 
 let display = document.getElementById('display');
 display.textContent = displayValue;
@@ -21,72 +21,142 @@ nonNumButtons.forEach(button => {
     button.addEventListener('click', nonNumFunction);
 })
 
+//
+//PREVENT DOUBLE OPERATORS FROM BEING CLICKED
+//ROUND ANSWERS TO 2 DECIMALS
+//LIMIT DISPLAY AND NUM VARIABLES SO THEY DON'T RUN OFF THE SCREEN
+// *******SET UP A FUNCTION THAT CHECKS IF THE CLICKED BUTTON IS AN OPERATOR AND IF IT IS DISABLE
+// THE OPERATOR BUTTONS, AND IF IT'S NOT THEN MAKE ALL OPERATORS ENABLED.USE checkButtons() and
+// toggleButtons()*******
+
+
 function nonNumFunction(event){
     const clickedButton = event.target;
     if( clickedButton.textContent === "clear") {
         reset();
     }
     if( clickedButton.textContent === "=") {
-        operate();                      // YOU NEED TO ADD FUNCTION THAT UPDATES THE DISPLAY
-    }                                   // AFTER DOING THE OPERATION ******
+        if( num1 && num2 && operator) {
+            operate(operator, parseFloat(num1), parseFloat(num2));
+        }     
+        console.log(num1, num2, operator)              
+    }                                  
 console.log(clickedButton.textContent)
 }
 function updateOperator(event){
     const clickedButton = event.target;
-    if( clickedButton.textContent === '+' || 
-        clickedButton.textContent === '-' || 
-        clickedButton.textContent === '*' ||
-        clickedButton.textContent === '/' ){
+    if(!num1) {
+        num1 = '0'
+    }
+    if(operator) {
+        operate(operator, parseFloat(num1), parseFloat(num2));
         operator = clickedButton.textContent;
-        display.textContent = displayValue + ' ' + operator;
-
-        operatorButtons.forEach(button => {
-            button.disabled = true;
-        })
-    }console.log(operator)
+    }
+    else if(clickedButton.textContent === '+' || 
+            clickedButton.textContent === '-' || 
+            clickedButton.textContent === '*' ||
+            clickedButton.textContent === '/' ){
+        operator = clickedButton.textContent;
+    }
+    toggleButtons(operatorButtons);
+    display.textContent = displayValue + ' ' + operator;
+    console.log("num1=",num1, "num2=",num2, "op=",operator, "dv=",displayValue)
+}
+function operatorCheck(clickedButton) {
+    if(Array.from(operatorButtons).includes(clickedButton)) {
+        toggleButtons(operatorButtons);
+    }
+    console.log('check ran');
+}
+function toggleButtons(buttonArray) {
+    Array.from(buttonArray).forEach(button => {
+        button.disabled = !button.disabled
+        
+    })
+}
+function checkButtons(buttonArray) {
+    for (let i = 0; i < buttonArray.length; i++) {
+        if(buttonArray[i].disabled) {
+            return true;
+        }
+        return false;
+    }
 }
 function updateDisplay(event){
     const clickedButton = event.target;
     if(!operator){
         num1 = (num1 || "") + clickedButton.textContent;
         displayValue = num1
-        display.textContent = displayValue;
     }
     else {
         num2 = (num2 || "") + clickedButton.textContent;
-        display.textContent = displayValue + " " + operator + " " + num2;
+        displayValue = num1 + " " + operator + " " + num2;
     }
-    console.log(num1, num2, operator)
+    if(checkButtons(operatorButtons)) {
+        toggleButtons(operatorButtons);
+        console.log('ran')
+    }
+    display.textContent = displayValue;
+    console.log("num1=",num1, "num2=",num2, "op=",operator, "dv=",displayValue)
 }
 function reset(){
     num1 = "";
     num2 = "";
     operator = "";
-    display.textContent = "000";
+    displayValue = "0";
+    console.log(num1, num2, operator)
+    display.textContent = displayValue;
+    if(checkButtons(numButtons)) {
+        toggleButtons(numButtons);
+        toggleButtons(operatorButtons);
+    }
 }
-function operate(operator, a, b){
-    if(operator == "+"){
-        return add(a, b)
+function operate(op, a, b){
+    if(op == "+"){
+        displayValue = add(a, b);
+        num1 = displayValue;
+        num2 = ""; 
+        operator = "";
     }
-    if(operator == "-"){
-        return subtract(a, b)
+    if(op == "-"){
+        displayValue = subtract(a, b);
+        num1 = displayValue;
+        num2 = "";
+        operator = "";
     }
-    if(operator == "*"){
-        return multiply(a, b)
+    if(op == "*"){
+        displayValue = multiply(a, b);
+        num1 = displayValue;
+        num2 = "";
+        operator = "";
     }
-    if(operator == "/"){
-        return divide(a, b)
+    if(op == "/"){
+        displayValue = divide(a, b);
+        num1 = displayValue;
+        num2 = "";
+        operator = "";
     }
+    display.textContent = num1;
+    console.log(displayValue, answer)
 }
 function add(a, b){
-    return a + b
+   answer = a + b; 
+   return answer
 }
 function subtract(a, b){
-    return a - b
+    answer = a - b;
+    return answer
 }
 function multiply(a, b){
-    return a * b
+    answer = a * b;
+    return answer
 }
 function divide(a, b){
-    return a / b
+    answer = a / b;
+    if(b == "0") {
+        toggleButtons(numButtons);
+        toggleButtons(operatorButtons);
+        return display.textContent = "Can't divide by 0";
+    }
+    return answer;
 }
